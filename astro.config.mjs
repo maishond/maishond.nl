e711f4b4726fe8a0a14a95c96952c490ab6af339
wouter <wouter@debruijn.dev>
2023-04-19 18:13:23 +0200

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  site: "https://maishond.nl",
+  integrations: [react(), tailwind(), sitemap()],
 });
